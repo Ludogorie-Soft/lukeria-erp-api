8c7dbf345946158f418dde25265f9595e8ffc4b0
Berkant <berki9@icloud.com>
2023-07-07 17:02:18 +0300

    adding necessary dependencies

---
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/LukeriaErpApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ludogoriesoft.lukeriaerpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukeriaErpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukeriaErpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/LukeriaErpApiApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.ludogoriesoft.lukeriaerpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukeriaErpApiApplicationTests {
+
+}
