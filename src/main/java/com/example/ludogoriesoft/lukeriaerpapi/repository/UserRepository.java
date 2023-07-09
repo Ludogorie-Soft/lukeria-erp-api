c9b28dddb90e57a4cb48a53e42182ca69b2ead4c
Berkant <berki9@icloud.com>
2023-07-09 21:06:08 +0300

    fix TODO messages in project add test for User and Carton

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/repository/UserRepository.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/repository/UserRepository.java
@@ -2,6 +2,14 @@ package com.example.ludogoriesoft.lukeriaerpapi.repository;
 
 import com.example.ludogoriesoft.lukeriaerpapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    List<User> findByDeletedFalse();
+
+    Optional<User> findByIdAndDeletedFalse(Long id);
 }
