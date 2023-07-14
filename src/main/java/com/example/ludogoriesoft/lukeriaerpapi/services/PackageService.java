5ce9bdc3e587f3c4eeb691af5c52038660e04310
Berkant <berki9@icloud.com>
2023-07-14 21:53:32 +0300

    move the " private Plate plateId; " field from Product to Package

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/PackageService.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/PackageService.java
@@ -56,6 +56,7 @@ public class PackageService {
         } else {
             throw new ValidationException("Carton ID cannot be null!");
         }
+
         if (packageDTO.getPlateId() != null) {
             boolean plateExists = plateRepository.existsById(packageDTO.getPlateId());
             if (!plateExists) {
@@ -65,6 +66,7 @@ public class PackageService {
             throw new ValidationException("Plate ID cannot be null!");
         }
         Package packageEntity = packageRepository.save(modelMapper.map(packageDTO, Package.class));
+
         return modelMapper.map(packageEntity, PackageDTO.class);
     }
 
