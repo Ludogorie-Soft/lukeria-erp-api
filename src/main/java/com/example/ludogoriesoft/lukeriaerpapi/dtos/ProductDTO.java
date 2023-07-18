149838a79e40f28f49c42507b2a32a486a745c9c
Berkant <berki9@icloud.com>
2023-07-18 20:49:15 +0300

    create a Package ID field in a Product model

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/dtos/ProductDTO.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/dtos/ProductDTO.java
@@ -14,6 +14,7 @@ public class ProductDTO {
     private Long id;
     @Min(value = 1, message = "Цената не може да бъде отрицателно число!")
     private BigDecimal price;
+    private Long packageId;
     @Min(value = 1, message = "Наличните бройки не могат да бъдат отрицателно число!")
     private int availableQuantity;
 
