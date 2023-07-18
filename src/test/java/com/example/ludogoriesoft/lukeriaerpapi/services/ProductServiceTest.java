bb786ffea04b96809a88a2f4b1f36545074236d2
Berkant <berki9@icloud.com>
2023-07-18 20:49:58 +0300

    create a Package ID field in a Product model

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ProductServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ProductServiceTest.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
