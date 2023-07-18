3dd9b12c1a419a5a85236220d51fbf1992b42949
Berkant <berki9@icloud.com>
2023-07-18 19:24:16 +0300

    Refactor  Double prices to BigDecimal in Plate class

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/Plate.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/Plate.java
@@ -3,6 +3,8 @@ package com.example.ludogoriesoft.lukeriaerpapi.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -15,7 +17,7 @@ public class Plate {
     @Column(name = "available_quantity")
     private Integer availableQuantity;
     private String photo;
-    private double price;
+    private BigDecimal price;
     @Column(name = "is_deleted")
     private boolean deleted;
 }
--- src/main/resources/db/migration/V2__Create_Plate_Table.sql
+++ src/main/resources/db/migration/V2__Create_Plate_Table.sql
@@ -3,6 +3,6 @@ CREATE TABLE plate (
   name VARCHAR(255) NOT NULL,
   available_quantity INT,
   photo VARCHAR(255),
-  price DOUBLE,
+   price DECIMAL(10, 2),
   is_deleted TINYINT(1) DEFAULT FALSE
 );
