97f7667f3126a31625726374ec105d229fa92f4f
Berkant <berki9@icloud.com>
2023-07-25 23:00:55 +0300

    add tests and fix sonar gate

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/MaterialOrderServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/MaterialOrderServiceTest.java
@@ -578,4 +578,33 @@ class MaterialOrderServiceTest {
         // Проверка за хвърляне на изключение
         assertThrows(ValidationException.class, () -> materialOrderService.getAllOrderProductsByOrderId(1L), "Invalid Package ID");
     }
+
+    @Test
+    void testGetAllOrderProducts235ByOrderId() {
+        // Подгответе мокнати данни, които ще върне orderProductRepository.findAll()
+        Order order = new Order();
+        order.setId(1L);
+
+        Package packageEntity = new Package();
+        packageEntity.setPiecesCarton(10);
+        packageEntity.setAvailableQuantity(1);// Настройте стойността на piecesCarton на 10
+
+        Carton carton = mock(Carton.class);
+        when(carton.getAvailableQuantity()).thenReturn(100); // Примерно връща 100 налични кашони
+        packageEntity.setCartonId(carton);
+
+        Plate plate = mock(Plate.class);
+        when(plate.getAvailableQuantity()).thenReturn(100); // Примерно връща 50 налични тарелки
+        packageEntity.setPlateId(plate);
+
+        List<OrderProduct> orderProducts = Arrays.asList(
+                new OrderProduct(1L, 10, order, packageEntity, false),
+                new OrderProduct(2L, 10, order, packageEntity, false),
+                new OrderProduct(1L, 10, order, packageEntity, false)
+        );
+        when(orderProductRepository.findAll()).thenReturn(orderProducts);
+
+        // Проверка за хвърляне на изключение
+        assertThrows(ValidationException.class, () -> materialOrderService.getAllOrderProductsByOrderId(1L), "Invalid Package ID");
+    }
 }
