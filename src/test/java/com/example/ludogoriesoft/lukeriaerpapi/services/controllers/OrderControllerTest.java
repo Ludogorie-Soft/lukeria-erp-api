8b99142f772e28e373e7872d55d9d12352a9d143
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-08-02 10:20:42 +0300

    add OrderControllerIntegrationTest - 100% coverage

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/controllers/OrderControllerTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/controllers/OrderControllerTest.java
@@ -1,28 +1,60 @@
 package com.example.ludogoriesoft.lukeriaerpapi.services.controllers;
 
 import com.example.ludogoriesoft.lukeriaerpapi.controllers.OrderController;
+import com.example.ludogoriesoft.lukeriaerpapi.controllers.OrderController;
 import com.example.ludogoriesoft.lukeriaerpapi.dtos.OrderDTO;
+import com.example.ludogoriesoft.lukeriaerpapi.dtos.OrderDTO;
+import com.example.ludogoriesoft.lukeriaerpapi.exeptions.ApiExceptionHandler;
 import com.example.ludogoriesoft.lukeriaerpapi.models.Order;
 import com.example.ludogoriesoft.lukeriaerpapi.services.OrderService;
+import com.example.ludogoriesoft.lukeriaerpapi.services.OrderService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ValidationException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith({MockitoExtension.class, SpringExtension.class})
-@WebMvcTest(OrderController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(value = OrderController.class,
+        useDefaultFilters = false,
+        includeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        value = OrderController.class),
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        value = ApiExceptionHandler.class
+                )
+        }
+)
 class OrderControllerTest {
 
     @Autowired
@@ -31,12 +63,9 @@ class OrderControllerTest {
     @MockBean
     private OrderService orderService;
 
-    private Order order;
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @BeforeEach
     public void setup() {
-        order = new Order();
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
@@ -48,5 +77,181 @@ class OrderControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(new ObjectMapper().writeValueAsString(orderDTO)));
+    }
+    @Test
+    void testGetAllOrders() throws Exception {
+        OrderDTO orderDTO1 = new OrderDTO();
+        orderDTO1.setId(1L);
+        OrderDTO orderDTO2 = new OrderDTO();
+        orderDTO2.setId(2L);
+        List<OrderDTO> orderDTOList = Arrays.asList(orderDTO1, orderDTO2);
+
+        when(orderService.getAllOrders()).thenReturn(orderDTOList);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/order")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    void testGetOrderById() throws Exception {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setId(1L);
+
+        when(orderService.getOrderById(anyLong())).thenReturn(orderDTO);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/order/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    void testCreateOrder() throws Exception {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setId(1L);
+
+        when(orderService.createOrder(any(OrderDTO.class))).thenReturn(orderDTO);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/order")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\": 1}"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    void testUpdateOrder() throws Exception {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setId(1L);
+
+        when(orderService.updateOrder(anyLong(), any(OrderDTO.class))).thenReturn(orderDTO);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/order/{id}", 1)
+                        .content("{\"id\": 1}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    void testDeleteOrderById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/order/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Order with id: 1 has been deleted successfully!"));
+    }
+
+    @Test
+    void testGetAllOrdersWhenNoOrderExist() throws Exception {
+        when(orderService.getAllOrders()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/order")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isEmpty())
+                .andReturn();
+    }
+
+    @Test
+    void testGetOrderByIdWhenOrderDoesNotExist() throws Exception {
+        long orderId = 1L;
+        when(orderService.getOrderById(orderId)).thenThrow(new ChangeSetPersister.NotFoundException());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/order/{id}", orderId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(containsString("Not found!")));
+    }
+
+    @Test
+    void testShouldNotCreateOrderWithBlankOrderName() throws Exception {
+        String blankOrderName = "";
+
+        doThrow(new ValidationException())
+                .when(orderService).createOrder(any(OrderDTO.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/order")
+                        .content("{\"id\": 1, \"" + blankOrderName + "\"}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void testGetOrderWithInvalidId() throws Exception {
+        Long invalidId = 100000L;
+
+        when(orderService.getOrderById(invalidId))
+                .thenThrow(new ChangeSetPersister.NotFoundException());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/order/{id}", invalidId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(containsString("Not found!")));
+    }
+
+    @Test
+    void testUpdateOrderWithInvalidData() throws Exception {
+        String invalidData = "";
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/order/{id}", 1)
+                        .content("{\"id\": 1, \": " + invalidData + "}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testUpdateOrderWithInvalidIdShouldReturnNotFound() throws Exception {
+        Long id = 1L;
+
+        OrderDTO updatedOrder = new OrderDTO();
+
+        when(orderService.updateOrder(eq(id), any(OrderDTO.class)))
+                .thenThrow(new ChangeSetPersister.NotFoundException());
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/order/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(updatedOrder)))
+                .andExpect(status().isNotFound());
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testDeleteOrderByIdWhenOrderDoesNotExist() throws Exception {
+        long orderId = 1L;
+        doThrow(new ChangeSetPersister.NotFoundException())
+                .when(orderService).deleteOrder(orderId);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/order/{id}", orderId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(containsString("Not found!")));
     }
 }
