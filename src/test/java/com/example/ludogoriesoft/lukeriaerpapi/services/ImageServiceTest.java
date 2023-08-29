1bb7bf0e0b17af53e10c16c43a59089e23d098dd
Berkant <berki9@icloud.com>
2023-08-29 10:14:46 +0300

    add tests

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ImageServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ImageServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.ludogoriesoft.lukeriaerpapi.services;
 
+import com.example.ludogoriesoft.lukeriaerpapi.models.Package;
 import com.example.ludogoriesoft.lukeriaerpapi.repository.PackageRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +10,11 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -20,12 +25,30 @@ class ImageServiceTest {
     @Mock
     private PackageRepository packageRepository;
 
-    private final String imageDirectory = "src/main/resources/static/PackageImages/";
-
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        imageService = new ImageService(packageRepository);
+        String imageDirectory = "src/main/resources/static/PackageImages/";
+        imageService = new ImageService(imageDirectory, packageRepository);
+    }
+
+    @Test
+    void testEditImageForPackage() throws IOException {
+        Long packageId = 1L;
+        Package aPackage = new Package();
+        when(packageRepository.findByIdAndDeletedFalse(packageId)).thenReturn(Optional.of(aPackage));
+
+        MockMultipartFile file = new MockMultipartFile(
+                "test-image.jpg",
+                "test-image.jpg",
+                "image/jpeg",
+                "Test image content".getBytes()
+        );
+
+        String uniqueFilename = imageService.editImageForPackage(file, packageId);
+
+        Assertions.assertNotNull(aPackage); // Проверка за null стойност
+        Assertions.assertEquals(uniqueFilename, aPackage.getPhoto());
     }
 
     @Test
@@ -79,20 +102,34 @@ class ImageServiceTest {
     }
 
 
-    @Test
-    void testGetImageBytesThrowsIOException() {
-        String imageName = "non-existent-image.jpg"; // Подразбира се, че това изображение не съществува
-
-        assertThrows(NullPointerException.class, () -> {
-            imageService.getImageBytes(imageName);
-        });
-    }
 
     @Test
     void testGenerateUniqueFilename() {
         String originalFilename = "example.jpg";
         String uniqueFilename = imageService.generateUniqueFilename(originalFilename);
         Assertions.assertTrue(uniqueFilename.matches("^\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12}_" + originalFilename + "$"));
+    }
+
+    @Test
+    void testGetImageBytes() throws IOException {
+        String imageName = "test-image.jpg";
+        String imageDirectory="src/main/resources/static/PackageImages/";
+        Path imagePath = Path.of(imageDirectory, imageName);
+        Files.createDirectories(imagePath.getParent());
+        Files.write(imagePath, "Test image content".getBytes());
+
+        byte[] imageBytes = imageService.getImageBytes(imageName);
+
+        Assertions.assertArrayEquals("Test image content".getBytes(), imageBytes);
+    }
+
+    @Test
+    void testGetImageBytesNonExistentImage() throws IOException {
+        String nonExistentImageName = "non-existent-image.jpg"; // Подразбира се, че това изображение не съществува
+
+        byte[] imageBytes = imageService.getImageBytes(nonExistentImageName);
+
+        Assertions.assertArrayEquals(new byte[0], imageBytes);
     }
 }
 
