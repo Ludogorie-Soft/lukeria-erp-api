8d15818bf9c1c99f8b571eb589c04867b89776f1
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-08-29 10:25:28 +0300

    delete no needed static folder

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ImageServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ImageServiceTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.nio.file.Path;
-import static org.testng.AssertJUnit.*;
 class ImageServiceTest {
     private ImageService imageService;
 
@@ -22,11 +21,5 @@ class ImageServiceTest {
         imageService = new ImageService(packageRepository);
     }
 
-    @Test
-    void testGenerateUniqueFilename() {
-        String originalFilename = "example.jpg";
-        String uniqueFilename = imageService.generateUniqueFilename(originalFilename);
-        assertTrue(uniqueFilename.matches("^\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12}_" + originalFilename + "$"));
-    }
 }
 
