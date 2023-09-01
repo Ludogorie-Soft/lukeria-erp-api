0879322a105aede82ea914fb497784631e5b3194
Berkant <berki9@icloud.com>
2023-09-01 08:54:16 +0300

    FIRST_INVOICE_NUMBER

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/InvoiceService.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/InvoiceService.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class InvoiceService {
     private final InvoiceRepository invoiceRepository;
     private final ModelMapper modelMapper;
-    public static final Long FIRS_INVOICE_NUMBER = 2000000000L;
+    public static final Long FIRST_INVOICE_NUMBER = 2000000000L;
 
     public Long findLastInvoiceNumberStartingWithTwo()  {
         String prefix = "2";
@@ -26,7 +26,7 @@ public class InvoiceService {
         List<String> lastInvoiceNumbers = invoiceRepository.findLastInvoiceNumberStartingWith(prefix);
 
         if (lastInvoiceNumbers.isEmpty()) {
-           return FIRS_INVOICE_NUMBER;
+           return FIRST_INVOICE_NUMBER;
         }
 
         String maxLastDigitNumber = lastInvoiceNumbers.stream()
