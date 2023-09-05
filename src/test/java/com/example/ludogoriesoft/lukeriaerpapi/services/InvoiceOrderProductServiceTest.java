f39afbe3f90eff5e8119075b04e1750ccff28edf
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-09-05 17:36:38 +0300

    last change - delete tests which are not from this branch

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/InvoiceOrderProductServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/InvoiceOrderProductServiceTest.java
@@ -23,6 +23,7 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.testng.AssertJUnit.assertNotNull;
 
 class InvoiceOrderProductServiceTest {
 
