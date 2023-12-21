9e4cd95283b88b9fed79e595fe939cc9d4767432
berki0 <107307903+berki0@users.noreply.github.com>
2023-12-22 00:05:26 +0200

    add .sql migrations

---
+++ src/main/resources/db/migration/V22__Insert_Test_User.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `address`, `username`, `role`, `is_deleted`) VALUES (NULL, 'root', 'root', 'root@mail.com', '$2a$12$tydoA7wP.LxYY0mr/MK29.nykzUlRbMZQxs5UnctDTNHS6LWtgsCu', 'root', 'root', 'ADMIN', '0');
