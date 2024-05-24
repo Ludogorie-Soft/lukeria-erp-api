1c4588a1cb1738843ca585b7e165e39c931f1afa
berki0 <107307903+berki0@users.noreply.github.com>
2024-05-24 20:46:03 +0300

    Change Edit options for Plate

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/InvoiceService.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/InvoiceService.java
@@ -15,8 +15,8 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class InvoiceService {
-    public static final Long FIRST_INVOICE_NUMBER = 2000001330L;
-    public static final Long FIRST_INVOICE_NUMBER_ABROAD = 1000000297L;
+    public static final Long FIRST_INVOICE_NUMBER = 2000001365L;
+    public static final Long FIRST_INVOICE_NUMBER_ABROAD = 1000000300L;
     private final InvoiceRepository invoiceRepository;
     private final ModelMapper modelMapper;
 
