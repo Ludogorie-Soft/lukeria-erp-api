ea2fd6bcb14eafd024722cf1e6b15a2c5e793671
first-branch <radeva7609@gmaill.com>
2024-09-10 16:12:37 +0300

    add integration tests for endpoints that a user is permitted to with USER role

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/dtos/ProductDTO.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/dtos/ProductDTO.java
@@ -20,4 +20,12 @@ public class ProductDTO {
     private int availableQuantity;
     @Size(min = 2, max = 10, message = "Кода на продукта трябда да бъде между 2 и 10 символа!")
     private String productCode;
+    public ProductDTO(BigDecimal price, Long packageId, int availableQuantity, String productCode) {
+        this.price = price;
+        this.packageId = packageId;
+        this.availableQuantity = availableQuantity;
+        this.productCode = productCode;
+    }
 }
+
+
--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/controllers/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/controllers/ProductControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import jakarta.validation.ValidationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,19 +21,23 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc(addFilters = false)
@@ -273,6 +278,63 @@ class ProductControllerIntegrationTest {
                         .header(HttpHeaders.AUTHORIZATION, "your-authorization-token") // Add the Authorization header
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    @Test
+    @WithMockUser(roles = "USER")
+    void testGetAllProductsWithUserRole() throws Exception {
+        ProductDTO productDTO1 = new ProductDTO();
+        productDTO1.setId(1L);
+        ProductDTO productDTO2 = new ProductDTO();
+        productDTO2.setId(2L);
+        List<ProductDTO> productDTOList = Arrays.asList(productDTO1, productDTO2);
+
+        when(productService.getAllProducts()).thenReturn(productDTOList);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "your-authorization-token") // Add the Authorization header
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void testGetProductByIdWithUSerRole() throws Exception {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(1L);
+
+        when(productService.getProductById(anyLong())).thenReturn(productDTO);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "your-authorization-token") // Add the Authorization header
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andReturn();
+
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")  // Simulate a user with ROLE_USER
+    public void testGetProductByIdNotFoundWithUserRole() throws Exception {
+        // Mock service to throw NotFoundException
+        Mockito.when(productService.getProductById(99L))
+                .thenThrow(new ChangeSetPersister.NotFoundException());
+
+        // Perform GET request and expect 404 Not Found
+        mockMvc.perform(get("/api/v1/product/99")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer valid-jwt-token"))
+                .andExpect(status().isNotFound());
     }
 }
 
