0d3847d27908a2a034e41b03daca10c9ce20a75b
berki0 <107307903+berki0@users.noreply.github.com>
2024-09-11 11:23:34 +0300

    fix the Sonar

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/config/SecurityConfiguration.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ package com.example.ludogoriesoft.lukeriaerpapi.config;
 import com.example.ludogoriesoft.lukeriaerpapi.handler.JwtAuthenticationEntryPoint;
 import com.example.ludogoriesoft.lukeriaerpapi.services.security.JwtAuthenticationFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.slack.api.audit.Actions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +31,16 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
     private final ObjectMapper objectMapper;
-
+    public static final String CLIENT_URL = "/api/v1/client/**";
+    public static final String PACKAGE_URL = "/api/v1/package/**";
+    public static final String PLATE_URL = "/api/v1/plate/**";
+    public static final String CARTON_URL = "/api/v1/carton/**";
+    public static final String PRODUCT_URL = "/api/v1/product/**";
+    public static final String ORDER_URL = "/api/v1/order/**";
+    public static final String ORDER_PRODUCT_URL = "/api/v1/orderProduct/**";
+    public static final String MONTHLY_ORDER_URL = "/api/v1/monthlyOrder/**";
+    public static final String MONTHLY_ORDER_PRODUCT_URL = "/api/v1/monthlyOrderProduct/**";
+    public static final String INVOICE_URL = "/api/v1/monthlyOrderProduct/**";
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -51,60 +59,60 @@ public class SecurityConfiguration {
                 .permitAll()
 
                 .requestMatchers("/api/v1/user/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.GET, "/api/v1/client/**").hasAnyRole(ADMIN.name(), PRODUCTION_MANAGER.name(), TRANSPORT_MANAGER.name(), USER.name())
+                .requestMatchers(HttpMethod.GET, CLIENT_URL).hasAnyRole(ADMIN.name(), PRODUCTION_MANAGER.name(), TRANSPORT_MANAGER.name(), USER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/client", "/api/v1/upload/file").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/client/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/client/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, CLIENT_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, CLIENT_URL).hasAnyRole(ADMIN.name())
 
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/package/**").hasAnyRole(TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name(), ADMIN.name(), USER.name())
+                .requestMatchers(HttpMethod.GET, PACKAGE_URL).hasAnyRole(TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name(), ADMIN.name(), USER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/package").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/package/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/package/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, PACKAGE_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, PACKAGE_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/plate/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(),TRANSPORT_MANAGER.name(), USER.name())
+                .requestMatchers(HttpMethod.GET, PLATE_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(),TRANSPORT_MANAGER.name(), USER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/plate").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/plate/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/plate/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, PLATE_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, PLATE_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/carton/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(),TRANSPORT_MANAGER.name(), USER.name())
+                .requestMatchers(HttpMethod.GET, CARTON_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(),TRANSPORT_MANAGER.name(), USER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/carton").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/carton/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/carton/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, CARTON_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, CARTON_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/product/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name(), USER.name())
+                .requestMatchers(HttpMethod.GET, PRODUCT_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name(), USER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/product").hasAnyRole(ADMIN.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/product/produce/***").hasAnyRole(ADMIN.name(), PRODUCTION_MANAGER.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/product/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/product/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, PRODUCT_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, PRODUCT_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/order/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.GET, ORDER_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/order").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/order/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/order/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, ORDER_URL).hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.DELETE, ORDER_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/orderProduct/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.POST, "/api/v1/orderProduct/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/orderProduct/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/orderProduct/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.GET, ORDER_PRODUCT_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.POST, ORDER_PRODUCT_URL).hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.PUT, ORDER_PRODUCT_URL).hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.DELETE, ORDER_PRODUCT_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/monthlyOrder/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.GET, MONTHLY_ORDER_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/monthlyOrder").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/monthlyOrder/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/monthlyOrder/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, MONTHLY_ORDER_URL).hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
+                .requestMatchers(HttpMethod.DELETE, MONTHLY_ORDER_URL).hasAnyRole(ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/monthlyOrderProduct/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
+                .requestMatchers(HttpMethod.GET, MONTHLY_ORDER_PRODUCT_URL).hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name(), TRANSPORT_MANAGER.name())
                 .requestMatchers(HttpMethod.POST, "/api/v1/monthlyOrderProduct").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/monthlyOrderProduct/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/monthlyOrderProduct/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, MONTHLY_ORDER_PRODUCT_URL).hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
+                .requestMatchers(HttpMethod.DELETE, MONTHLY_ORDER_PRODUCT_URL).hasAnyRole(ADMIN.name())
 
                 .requestMatchers("/api/v1/material-order/**").hasAnyRole(PRODUCTION_MANAGER.name(), ADMIN.name())
 
-                .requestMatchers(HttpMethod.GET, "/api/v1/invoice/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
+                .requestMatchers(HttpMethod.GET, INVOICE_URL).hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
                 .requestMatchers(HttpMethod.GET, "/api/v1/invoice/number/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
-                .requestMatchers(HttpMethod.POST, "/api/v1/invoice/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.PUT, "/api/v1/invoice/**").hasAnyRole(ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/invoice/**").hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.POST, INVOICE_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.PUT, INVOICE_URL).hasAnyRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE, INVOICE_URL).hasAnyRole(ADMIN.name())
                 .requestMatchers("/api/v1/invoiceOrderProduct/**").hasAnyRole(ADMIN.name(), TRANSPORT_MANAGER.name(), PRODUCTION_MANAGER.name())
                 .requestMatchers(HttpMethod.POST,"/api/v1/upload/**").hasAnyRole(ADMIN.name())
                 .anyRequest()
