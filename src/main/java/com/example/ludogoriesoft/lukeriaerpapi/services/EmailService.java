b2de1d29fbbc6f70f04db99a3cf8876a8b048247
berki0 <107307903+berki0@users.noreply.github.com>
2024-09-12 15:58:33 +0300

    problem with PasswordResetToken when we try to save it

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/EmailService.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/services/EmailService.java
@@ -1,6 +1,5 @@
 package com.example.ludogoriesoft.lukeriaerpapi.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,14 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
     private JavaMailSender mailSender;
 
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
 
     public void sendSimpleEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("berki07092@gmail.com");  // Email адресът, от който се изпраща
+        message.setFrom("berki07092@gmail.com");
         message.setTo(toEmail);
         message.setSubject(subject);
         message.setText(body);
