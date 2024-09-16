405fe958ebc9f9451ba9654997bcdad95ad53cbe
berki0 <107307903+berki0@users.noreply.github.com>
2024-09-16 11:26:17 +0300

    fix tests after fix ApiExceptionHandler

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/exeptions/ApiExceptionHandler.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/exeptions/ApiExceptionHandler.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,12 +24,6 @@ public class ApiExceptionHandler {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<String> handleValidationException(ValidationException ex) {
         String errorMessage = "Validation error: " + ex.getMessage();
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        String errorMessage = "Validation failed: " + ex.getBindingResult().getFieldError().getDefaultMessage();
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
