5704d4f22955c10a5c479da3de7193d2bd67d172
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-09-19 21:20:57 +0300

    update integration tests

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/controllers/CustomerCustomPriceControllerIntegrationTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/controllers/CustomerCustomPriceControllerIntegrationTest.java
@@ -3,6 +3,8 @@ package com.example.ludogoriesoft.lukeriaerpapi.services.controllers;
 import com.example.ludogoriesoft.lukeriaerpapi.controllers.CustomerCustomPriceController;
 import com.example.ludogoriesoft.lukeriaerpapi.dtos.CustomerCustomPriceDTO;
 import com.example.ludogoriesoft.lukeriaerpapi.exeptions.ApiExceptionHandler;
+import com.example.ludogoriesoft.lukeriaerpapi.models.Client;
+import com.example.ludogoriesoft.lukeriaerpapi.models.Product;
 import com.example.ludogoriesoft.lukeriaerpapi.services.CustomerCustomPriceService;
 import com.example.ludogoriesoft.lukeriaerpapi.slack.SlackService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,12 +27,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,8 +48,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         }
 )
 public class CustomerCustomPriceControllerIntegrationTest {
+
     @Autowired
     private MockMvc mockMvc;
+
     @MockBean
     private SlackService slackService;
 
@@ -76,10 +75,14 @@ public class CustomerCustomPriceControllerIntegrationTest {
     void testGetAllCustomPrices() throws Exception {
         CustomerCustomPriceDTO price1 = new CustomerCustomPriceDTO();
         price1.setId(1L);
+        price1.setClientId(1L);
+        price1.setProductId(1L);
         price1.setPrice(BigDecimal.valueOf(100.0));
 
         CustomerCustomPriceDTO price2 = new CustomerCustomPriceDTO();
         price2.setId(2L);
+        price2.setClientId(1L);
+        price2.setProductId(2L);
         price2.setPrice(BigDecimal.valueOf(200.0));
 
         List<CustomerCustomPriceDTO> prices = Arrays.asList(price1, price2);
@@ -101,6 +104,8 @@ public class CustomerCustomPriceControllerIntegrationTest {
     void testCreateCustomPriceForCustomer() throws Exception {
         CustomerCustomPriceDTO newPrice = new CustomerCustomPriceDTO();
         newPrice.setId(1L);
+        newPrice.setClientId(1L);
+        newPrice.setProductId(1L);
         newPrice.setPrice(BigDecimal.valueOf(150.0));
 
         when(customerCustomPriceService.create(any(CustomerCustomPriceDTO.class))).thenReturn(newPrice);
@@ -118,8 +123,11 @@ public class CustomerCustomPriceControllerIntegrationTest {
     void testUpdateCustomPrice() throws Exception {
         CustomerCustomPriceDTO updatedPrice = new CustomerCustomPriceDTO();
         updatedPrice.setId(1L);
+        updatedPrice.setClientId(1L); // Client ID
+        updatedPrice.setProductId(1L); // Product ID
         updatedPrice.setPrice(BigDecimal.valueOf(175.0));
 
+        // Mock the update method
         when(customerCustomPriceService.update(any(CustomerCustomPriceDTO.class))).thenReturn(updatedPrice);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/customerCustomPrice/")
@@ -135,6 +143,8 @@ public class CustomerCustomPriceControllerIntegrationTest {
     void testDeleteCustomPrice() throws Exception {
         CustomerCustomPriceDTO deletedPrice = new CustomerCustomPriceDTO();
         deletedPrice.setId(1L);
+        deletedPrice.setClientId(1L);
+        deletedPrice.setProductId(1L);
         deletedPrice.setPrice(BigDecimal.valueOf(100.0));
 
         when(customerCustomPriceService.delete(anyLong(), anyLong())).thenReturn(deletedPrice);
@@ -166,69 +176,45 @@ public class CustomerCustomPriceControllerIntegrationTest {
     void testCreateCustomPriceWhenNotFound() throws Exception {
         CustomerCustomPriceDTO priceDTO = new CustomerCustomPriceDTO();
         priceDTO.setId(1L);
+        priceDTO.setClientId(1L);
+        priceDTO.setProductId(1L);
         priceDTO.setPrice(BigDecimal.valueOf(100.0));
 
-        doThrow(new ChangeSetPersister.NotFoundException()).when(customerCustomPriceService).create(any(CustomerCustomPriceDTO.class));
+        when(customerCustomPriceService.create(any(CustomerCustomPriceDTO.class))).thenThrow(new ChangeSetPersister.NotFoundException());
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/customerCustomPrice")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer your-authorization-token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(priceDTO)))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string(containsString("Not found!")));
+                .andExpect(status().isNotFound());
     }
 
     @Test
     void testUpdateCustomPriceWhenNotFound() throws Exception {
         CustomerCustomPriceDTO priceDTO = new CustomerCustomPriceDTO();
         priceDTO.setId(1L);
+        priceDTO.setClientId(1L);
+        priceDTO.setProductId(1L);
         priceDTO.setPrice(BigDecimal.valueOf(175.0));
 
-        doThrow(new ChangeSetPersister.NotFoundException()).when(customerCustomPriceService).update(any(CustomerCustomPriceDTO.class));
+        when(customerCustomPriceService.update(any(CustomerCustomPriceDTO.class))).thenThrow(new ChangeSetPersister.NotFoundException());
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/customerCustomPrice/")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer your-authorization-token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(priceDTO)))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string(containsString("Not found!")));
+                .andExpect(status().isNotFound());
     }
 
     @Test
     void testDeleteCustomPriceWhenNotFound() throws Exception {
-        doThrow(new ChangeSetPersister.NotFoundException()).when(customerCustomPriceService).delete(anyLong(), anyLong());
+        when(customerCustomPriceService.delete(anyLong(), anyLong())).thenThrow(new ChangeSetPersister.NotFoundException());
 
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/customerCustomPrice/")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer your-authorization-token")
                         .param("clientId", "1")
                         .param("productId", "1")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string(containsString("Not found!")));
-    }
-
-    @Test
-    void testAllProductsWithAndWithoutCustomPrice() throws Exception {
-        CustomerCustomPriceDTO price1 = new CustomerCustomPriceDTO();
-        price1.setId(1L);
-        price1.setPrice(BigDecimal.valueOf(100.0));
-
-        CustomerCustomPriceDTO price2 = new CustomerCustomPriceDTO();
-        price2.setId(2L);
-        price2.setPrice(BigDecimal.valueOf(200.0));
-
-        List<CustomerCustomPriceDTO> prices = Arrays.asList(price1, price2);
-
-        when(customerCustomPriceService.allProductWithCustomPriceForClient(anyLong())).thenReturn(prices);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/customerCustomPrice/allForClient/{id}", 1L)
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer your-authorization-token")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(2))
-                .andExpect(jsonPath("$[0].id").value(1))
-                .andExpect(jsonPath("$[0].price").value(100.0))
-                .andExpect(jsonPath("$[1].id").value(2))
-                .andExpect(jsonPath("$[1].price").value(200.0));
+                .andExpect(status().isNotFound());
     }
 }
