f956d51aa44b3fbd5dd058b4b1c4d7dfb4cbec9b
Eri1905 <ery.mtl1905@gmail.com>
2024-09-22 23:04:03 +0300

    Little improvements

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/ClientUser.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/ClientUser.java
@@ -2,16 +2,19 @@ package com.example.ludogoriesoft.lukeriaerpapi.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.checkerframework.checker.units.qual.C;
 
 @Entity
 @Data
 @Table(name = "client_user")
 public class ClientUser {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long client_id;
-    private Long user_id;
+    @Column(name = "client_id")
+    private Long clientId;
+    @Column(name = "user_id")
+    private Long userId;
     @Column(name = "is_deleted")
     private boolean deleted;
 }
