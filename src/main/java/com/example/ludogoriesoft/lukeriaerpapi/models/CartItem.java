9fa9d2416b6c58ae51dcf25e07cbd283281f29a5
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-10-11 09:06:08 +0300

    Create CartItem and CartItemDTO

---
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/CartItem.java
@@ -0,0 +1,28 @@
+package com.example.ludogoriesoft.lukeriaerpapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product productId;
+    private int quantity;
+    private double price;
+
+}
