8871a0586e149991898090487b76386a92f5e98b
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-10-14 10:22:18 +0300

    Add migration

--- src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/CartItem.java
+++ src/main/java/com/example/ludogoriesoft/lukeriaerpapi/models/CartItem.java
@@ -4,11 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -19,10 +22,10 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product productId;
     private int quantity;
-    private double price;
-
+    private BigDecimal price;
+    private boolean deleted;
 }
