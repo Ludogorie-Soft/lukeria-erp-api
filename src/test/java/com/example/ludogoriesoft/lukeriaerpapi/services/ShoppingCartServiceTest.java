91eba3267db12374f5a2aed458947217d0a61f2b
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-11-21 00:29:47 +0200

    add more unit tests

--- src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ShoppingCartServiceTest.java
+++ src/test/java/com/example/ludogoriesoft/lukeriaerpapi/services/ShoppingCartServiceTest.java
@@ -13,12 +13,14 @@ import com.example.ludogoriesoft.lukeriaerpapi.repository.CustomerCustomPriceRep
 import com.example.ludogoriesoft.lukeriaerpapi.repository.ProductRepository;
 import com.example.ludogoriesoft.lukeriaerpapi.repository.ShoppingCartRepository;
 import jakarta.validation.ValidationException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -30,6 +32,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
 public class ShoppingCartServiceTest {
     @Mock
     private UserService userService;
@@ -52,6 +55,168 @@ public class ShoppingCartServiceTest {
     @BeforeEach
     void setup() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testAddToCart_ProductAlreadyInCart_WithinAvailableQuantity() throws ChangeSetPersister.NotFoundException {
+        // Arrange
+        Long productId = 1L;
+        int quantityToAdd = 2;
+
+        UserDTO user = new UserDTO();
+        user.setId(1L);
+
+        Client client = new Client();
+        client.setId(1L);
+
+        ClientUser clientUser = new ClientUser();
+        clientUser.setClient(client);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setClientId(client);
+        List<CartItem> cartItems = new ArrayList<>();
+        Product product = new Product();
+        product.setId(productId);
+        product.setAvailableQuantity(10);
+        CartItem cartItem = new CartItem();
+        cartItem.setProductId(product);
+        cartItem.setQuantity(3);
+        cartItems.add(cartItem);
+
+        shoppingCart.setItems(cartItems);
+
+        when(userService.findAuthenticatedUser()).thenReturn(user);
+        when(clientUserRepository.findByUserIdAndDeletedFalse(user.getId())).thenReturn(Optional.of(clientUser));
+        when(shoppingCartRepository.findByClientId(client)).thenReturn(Optional.of(shoppingCart));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        // Act
+        shoppingCartService.addToCart(productId, quantityToAdd);
+
+        // Assert
+        assertEquals(5, cartItem.getQuantity());
+        verify(cartItemRepository, never()).save(any());
+        verify(shoppingCartRepository, times(1)).save(shoppingCart);
+    }
+
+    @Test
+    void testAddToCart_ProductAlreadyInCart_ExceedsAvailableQuantity() throws ChangeSetPersister.NotFoundException {
+        // Arrange
+        Long productId = 1L;
+        int quantityToAdd = 8;
+
+        UserDTO user = new UserDTO();
+        user.setId(1L);
+
+        Client client = new Client();
+        client.setId(1L);
+
+        ClientUser clientUser = new ClientUser();
+        clientUser.setClient(client);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setClientId(client);
+        List<CartItem> cartItems = new ArrayList<>();
+        Product product = new Product();
+        product.setId(productId);
+        product.setAvailableQuantity(10);
+        CartItem cartItem = new CartItem();
+        cartItem.setProductId(product);
+        cartItem.setQuantity(3);
+        cartItems.add(cartItem);
+
+        shoppingCart.setItems(cartItems);
+
+        when(userService.findAuthenticatedUser()).thenReturn(user);
+        when(clientUserRepository.findByUserIdAndDeletedFalse(user.getId())).thenReturn(Optional.of(clientUser));
+        when(shoppingCartRepository.findByClientId(client)).thenReturn(Optional.of(shoppingCart));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        // Act & Assert
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> shoppingCartService.addToCart(productId, quantityToAdd)
+        );
+        assertEquals("There is no that much quantity", exception.getMessage());
+    }
+
+    @Test
+    void testAddToCart_ProductNotInCart_WithinAvailableQuantity() throws ChangeSetPersister.NotFoundException {
+        // Arrange
+        Long productId = 1L;
+        int quantityToAdd = 5;
+
+        UserDTO user = new UserDTO();
+        user.setId(1L);
+
+        Client client = new Client();
+        client.setId(1L);
+
+        ClientUser clientUser = new ClientUser();
+        clientUser.setClient(client);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setClientId(client);
+        shoppingCart.setItems(new ArrayList<>());
+
+        Product product = new Product();
+        product.setId(productId);
+        product.setAvailableQuantity(10);
+        product.setPrice(BigDecimal.valueOf(100.0));
+
+        when(userService.findAuthenticatedUser()).thenReturn(user);
+        when(clientUserRepository.findByUserIdAndDeletedFalse(user.getId())).thenReturn(Optional.of(clientUser));
+        when(shoppingCartRepository.findByClientId(client)).thenReturn(Optional.of(shoppingCart));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(cartItemRepository.save(any(CartItem.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Act
+        shoppingCartService.addToCart(productId, quantityToAdd);
+
+        // Assert
+        assertEquals(1, shoppingCart.getItems().size());
+        CartItem newCartItem = shoppingCart.getItems().get(0);
+        assertEquals(product, newCartItem.getProductId());
+        assertEquals(quantityToAdd, newCartItem.getQuantity());
+        assertEquals(BigDecimal.valueOf(100.0), newCartItem.getPrice());
+        verify(cartItemRepository, times(1)).save(newCartItem);
+        verify(shoppingCartRepository, times(1)).save(shoppingCart);
+    }
+
+    @Test
+    void testAddToCart_ProductNotInCart_ExceedsAvailableQuantity() throws ChangeSetPersister.NotFoundException {
+        // Arrange
+        Long productId = 1L;
+        int quantityToAdd = 15;
+
+        UserDTO user = new UserDTO();
+        user.setId(1L);
+
+        Client client = new Client();
+        client.setId(1L);
+
+        ClientUser clientUser = new ClientUser();
+        clientUser.setClient(client);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setClientId(client);
+        shoppingCart.setItems(new ArrayList<>());
+
+        Product product = new Product();
+        product.setId(productId);
+        product.setAvailableQuantity(10);
+
+        when(userService.findAuthenticatedUser()).thenReturn(user);
+        when(clientUserRepository.findByUserIdAndDeletedFalse(user.getId())).thenReturn(Optional.of(clientUser));
+        when(shoppingCartRepository.findByClientId(client)).thenReturn(Optional.of(shoppingCart));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        // Act & Assert
+        ValidationException exception = assertThrows(
+                ValidationException.class,
+                () -> shoppingCartService.addToCart(productId, quantityToAdd)
+        );
+        assertEquals("There is no that much quantity", exception.getMessage());
     }
 
     @Test
